abcdc04ddbbe66b4942ba4c5ceef27e012b5ad10
Christian Zufferey <christian@zufferey.com>
2024-10-02 11:14:54 +0200

    new: 2e sonde de température pour le banc tests WIFI du y-fablab

--- thermo-htu21d-mqtt-dsleep/thermo-htu21d-mqtt-dsleep.ino
+++ thermo-htu21d-mqtt-dsleep/thermo-htu21d-mqtt-dsleep.ino
@@ -3,8 +3,8 @@
 //
 // ATTENTION, ce code a été testé sur un esp32-c3 super mini. Pas testé sur les autres boards !
 //
-#define zVERSION        "zf241001.1728"
-#define zHOST           "y-fablab-th1"              // ATTENTION, tout en minuscule
+#define zVERSION        "zf241001.2022"
+#define zHOST           "y-fablab-th2"              // ATTENTION, tout en minuscule
 #define zDSLEEP         0                       // 0 ou 1 !
 #define TIME_TO_SLEEP   120                 // dSleep en secondes 
 int zDelay1Interval =   5000;              // Délais en mili secondes pour la boucle loop
